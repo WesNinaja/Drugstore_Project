b5ca9e03fd523bc2c6013f78f8d93c5fc66025d6
wesleycalle <wesleyninaja@gmail.com>
2021-12-11 22:28:43 -0300

    @WesNinaja [Task:3 Criando notações da entidade produto]

--- src/main/java/com/drogaria/drugstore/domain/Produto.java
+++ src/main/java/com/drogaria/drugstore/domain/Produto.java
@@ -3,7 +3,10 @@ package com.drogaria.drugstore.domain;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -15,13 +18,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="tb_produto")
+@Table(name = "tb_produto")
 public class Produto {
+
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Short codigo;
+
+	// Quero dizer que no nome o máximo de caracteres que poderei usar é 50
+	@Column(length = 50, nullable = false, unique = true)
 	private String nome;
+
+	@Column(nullable = false)
 	private Short quantidade;
-	private BigDecimal preco;
-	private LocalDate dataDeValidade;
 	
+	@Column(nullable = false, precision = 5, scale = 2)
+	private BigDecimal preco;
+	
+	@Column
+	private LocalDate dataDeValidade;
+
 }
